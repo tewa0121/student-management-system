f79adb7c30bc52a2e0b6e72fcac8df9585347301
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-28 21:01:36 +0300

    comment out

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,10 +1,10 @@
-// const errorHandler = (err, req, res, next) => {
-//   console.error('❌ Error:', err.stack);
-//   // Send detailed error in development (for debugging)
-//   res.status(500).json({
-//     message: err.message || 'Something went wrong!',
-//     stack: err.stack,
-//   });
-// };
+const errorHandler = (err, req, res, next) => {
+  console.error('❌ Error:', err.stack);
+  // Send detailed error in development (for debugging)
+  res.status(500).json({
+    message: err.message || 'Something went wrong!',
+    stack: err.stack,
+  });
+};
 
-// module.exports = errorHandler;
+module.exports = errorHandler;

9ad52d7a8f9ec024e0f2d3f709c29d4feb747a7c
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-28 21:02:12 +0300

    announcement

--- backend/src/routes/announcementRoutes.js
+++ backend/src/routes/announcementRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const { getAnnouncements, getAnnouncement, createAnnouncement, updateAnnouncement, deleteAnnouncement } = require('../controllers/announcementController');
-const { authenticate, authorize } = require('../middleware/auth');
+// const express = require('express');
+// const { getAnnouncements, getAnnouncement, createAnnouncement, updateAnnouncement, deleteAnnouncement } = require('../controllers/announcementController');
+// const { authenticate, authorize } = require('../middleware/auth');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.use(authenticate);
+// router.use(authenticate);
 
-router.get('/', authorize('announcements.view'), getAnnouncements);
-router.get('/:id', authorize('announcements.view'), getAnnouncement);
-router.post('/', authorize('announcements.create'), createAnnouncement);
-router.put('/:id', authorize('announcements.update'), updateAnnouncement);
-router.delete('/:id', authorize('announcements.delete'), deleteAnnouncement);
+// router.get('/', authorize('announcements.view'), getAnnouncements);
+// router.get('/:id', authorize('announcements.view'), getAnnouncement);
+// router.post('/', authorize('announcements.create'), createAnnouncement);
+// router.put('/:id', authorize('announcements.update'), updateAnnouncement);
+// router.delete('/:id', authorize('announcements.delete'), deleteAnnouncement);
 
-module.exports = router;
+// module.exports = router;

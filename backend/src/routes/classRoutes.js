4a65d1b126a8994e491e8e084f880ffddfb52ec9
tewa0121 <tewachewmelaku6@gmail.com>
2026-08-29 19:24:37 +0300

    class routes

--- backend/src/routes/classRoutes.js
+++ backend/src/routes/classRoutes.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const {
-  getClasses,
-  getClass,
-  createClass,
-  updateClass,
-  deleteClass,
-} = require('../controllers/classController');
-const { authenticate, authorize } = require('../middleware/auth');
+// const express = require('express');
+// const {
+//   getClasses,
+//   getClass,
+//   createClass,
+//   updateClass,
+//   deleteClass,
+// } = require('../controllers/classController');
+// const { authenticate, authorize } = require('../middleware/auth');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.use(authenticate);
+// router.use(authenticate);
 
-router.get('/', authorize('academics.view'), getClasses);
-router.get('/:id', authorize('academics.view'), getClass);
-router.post('/', authorize('academics.create'), createClass);
-router.put('/:id', authorize('academics.update'), updateClass);
-router.delete('/:id', authorize('academics.delete'), deleteClass);
+// router.get('/', authorize('academics.view'), getClasses);
+// router.get('/:id', authorize('academics.view'), getClass);
+// router.post('/', authorize('academics.create'), createClass);
+// router.put('/:id', authorize('academics.update'), updateClass);
+// router.delete('/:id', authorize('academics.delete'), deleteClass);
 
-module.exports = router;
+// module.exports = router;
